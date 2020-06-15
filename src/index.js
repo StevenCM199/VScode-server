361535ad69d8ab5b1ae09e9cfb9a9f031311161b
StevenCM199 <StevenCM199@gmail.com>
2020-06-15 05:11:12 -0600

    extension server

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+//settings
+app.set('port' , process.env.PORT || 3000);
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/')));
+
+
+//start the server
+const server = app.listen (app.get('port'), () => {
+  console.log('server on port' , app.get('port'));
+});
+
+//websockets
+const SocketIO = require ('socket.io');
+const io = SocketIO(server);
+
+
+io.on('connection', () => {
+  console.log('new connection');
+});
